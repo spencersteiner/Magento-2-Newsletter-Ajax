ac166c8b10820ad917a39b78b67265bc46699155
Spencer Steiner <spencer.steiner@protonmail.com>
2019-02-13 22:57:15 -0700

    Syntax and copyright

--- registration.php
+++ registration.php
@@ -1,6 +1,9 @@
 <?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'SS_NewsletterAjax',
-        __DIR__
-    );
+/**
+ * Copyright © Spencer Steiner.
+* See LICENSE.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'SS_NewsletterAjax', __DIR__);
